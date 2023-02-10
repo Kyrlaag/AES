dff451948bb9dd2305355a999e37880a4459b15a
aykutoksuz <aykutoksuz.124@gmail.com>
2023-02-10 14:49:49 +0300

    first commit

---
+++ AES/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace AES
+{
+    class Class1
+    {
+        public static string toEncode(string key,string text)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encoder = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memSt = new MemoryStream())
+                {
+                    using (CryptoStream crSt = new CryptoStream((Stream)memSt, encoder, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter stWr = new StreamWriter((Stream)crSt))
+                        {
+                            stWr.Write(text);
+                        }
+
+                        array = memSt.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string toDecode(string key, string text2)
+        {
+            byte[] iv = new byte[16];
+            byte[] aa = Convert.FromBase64String(text2);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform decoder = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoSt = new MemoryStream(aa))
+                {
+                    using (CryptoStream crySt = new CryptoStream((Stream)memoSt, decoder, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader strRead = new StreamReader((Stream)crySt))
+                        {
+                            return strRead.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AES/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter The Data:");
+            var str = Console.ReadLine();
+ 
+            Console.WriteLine("Enter a 32 digit key:");
+            var key = Console.ReadLine();
+
+            if (key.Length > 32)
+                key = key.Substring(0, 32);
+
+            else if (key.Length < 32)
+            {
+                for (; key.Length < 32;)
+                {
+                    key = key + key;
+
+                    if (key.Length > 32)
+                    {
+                        key = key.Substring(0, 32);
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Used Key=" + key);
+
+            var encodedString = Class1.toEncode(key,str);
+            Console.WriteLine("Encoded =" + encodedString);
+
+            var decodedString = Class1.toDecode(key, encodedString);
+            Console.WriteLine("Çözülmüş=" + decodedString);
+
+            Console.ReadKey();
+        }
+    }
+}
